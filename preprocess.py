e89de741c85bf670098e1c16ff8c966ee20a5d1e
joepdeeben <joep.deeben@student.uva.nl>
2023-05-19 03:37:34 +0200

    verbeterde CFG

--- preprocess.py
+++ preprocess.py
@@ -1,63 +1,76 @@
-import nltk, re, pprint
-from nltk import word_tokenize
+import nltk
+from nltk import word_tokenize, CFG
+from nltk.parse import RecursiveDescentParser
 from nltk import pos_tag
-from nltk import CFG
-from nltk.parse.generate import generate
-import random
+from nltk.tree import Tree
 
-f = open('corpus.txt', 'r')
-raw = f.readlines()
-rawstr = ''
+# Read the corpus
+with open('corpus.txt', 'r') as f:
+    raw = f.read()
 
+# Split the raw text into sentences
+sentences = raw.split('.')
 
-for sentence in raw:
-    rawstr += sentence.strip('\n') + " "
-
-tokenized = word_tokenize(rawstr)
-tagged = pos_tag(tokenized)
-
-#creëer de verschillende categorieën
-categories = []
-for word in tagged:
-    if word[1] in categories:
-        pass
-    else:
-        categories.append(word[1])
-
-#woorden categoriseren, dit is puur voor ons om een overzicht te geven
-words_checked = []
-for category in categories:
-    print(category, '->')
-    for word in tagged:
-        if (word[1] == category) and word not in words_checked:
-            print(f"'{word[0]}'|" )
-            words_checked.append(word)
-
+# Define the noun phrase pattern
 grammar = CFG.fromstring("""
-S -> VP
+S -> VP | DT NP VP | DT NP VP CC VP
+VP -> VB | VB NP | VB NP PP | VB PP | VB ADJP | VBP | VBD | VBZ
+NP -> NN | DT NN | CD NN | JJ NN | JJ NNS | NNS | PRP | NNP | PRPS NN | NNP NN | DT NNS
+PP -> IN NP
+ADJP -> JJ | JJ CC JJ 
 
-VP -> VB | VB NP | VB PP | VB NP PP | VB NP CC VP | VB PP CC VP
-
-NP -> DT N | DT NNS | DT N RP
-
-PP -> IN NP | IN NP PP | IN NP CC PP
-DT -> 'the' | 'a' | 'all' | 'every' | 'The' | 'this'|
-NNS -> 'bean' | 'food' | 'garlic' | 'table' | 'salt' | 'circle' | 'oil' | 'pan' | 'onion' | 'potato' | 'swede' | 'carrot' | 'parsnip' | 'water' | 'rice' | 'coriander' | 'tomato' | 'chicken' | 'semolina' | 'khoya' | 'dough' | 'ball' | 'mango' | 'sweetener' | 'yogurt' | 'container' | 'sorbet' | 'base' | 'mixture' | 'fork' | 'bowl' | 'pinch' | 'salt' | 'flour' | 'custard' | 'galen' | 'milk' | 'lemon' | 'whey' | 'cheese' | 'cloth' | 'ginger' | 'nut' | 'rice' | 'sugar' | 'cardamom' | 'pistachio' | 'almond'
-IN ->  'in' | 'of' | 'for' | 'so' | 'until' | 'after' | 'that' | 'into' | 'while' | 'with' | 'Once' | 'on' | 'if' | 'over' | 'After' | 'as' | 'For' | 'till' | 'before' | 'tard' | 'Boil' | 'As' | 'from' | 'under' | 'about' | 'If'
-VB -> 'leave' | 'add' | 'make' | 'fry' | 'mix' | 'heat' | 'simmer' | 'stir' | 'cook' | 'boil' | 'reduce' | 'check' | 'blend' | 'freeze' | 'beat' | 'scoop' | 'fill' | 'bring' | 'put' | 'separate' | 'beat' | 'pour' | 'turn' | 'wrap' | 'rinse' | 'squeeze' | 'transfer'
-CC -> 'and'| 'or'| 'but'
-RP -> 'up'| 'off'| 'out'|
-
+DT -> 'the' | 'a' | 'all' | 'Every' | 'every' | 'The' | 'This' | 'this'
+VB ->  'Return' | 'reheat' | 'Tip' | 'Seperate' | 'Stir' | 'Fill' | 'Pour' | 'Blend' | 'Keep' | 'Mix' | 'Cook' | 'Serve' | 'Cover' | 'Heat' | 'Fry' | 'Put'| 'be'| 'Make'| 'shape'| 'serve'| 'Place'| 'boil'| 'reduce'| 'Add'| 'Boil'| 'skin'| 'cut'| 'Chop'| 'cook'| 'check'| 'make'| 'separate'| 'let'| 'simmer'| 'Take'| 'soak'| 'set'| 'Beat'| 'return'| 'break'| 'Transfer'| 'fridge'| 'Buy'| 'burn'| 'stir'| 'start'| 'turn'| 'muslin'| 'Wrap'| 'take'| 'prevent'| 'scoop'| 'add'| 'cover'| 'cool'| 'have'| 'dip'| 'finish'| 'Repeat'| 'need'| 'do'| 'pan'
+NNS -> 'cheese' | 'beans'| 'hours'| 'ingredients'| 'carrots'| 'parsnips'| 'minutes'| 'cubes'| 'onions'| 'tomatoes'| 'khoya'| 'balls'| 'cups'| 'sides'| 'crystals'| 'mintures'| 'slices'| 'spoons'| 'boils'| 'cloth'| 'nuts'| 'cristals'| 'flavour'| 'pools'| 'seeds'| 'times'| 'pieces'| 'almonds'| 'pistachios'| 'yolks'| 'fingers'| 'tbsp'| 'layers'| 'mins'| 'batches'
+IN -> 'in'| 'of'| 'for'| 'so'| 'until'| 'after'| 'that'| 'into'| 'while'| 'with'| 'Once'| 'on'| 'if'| 'over'| 'After'| 'as'| 'For'| 'till'| 'before'| 'tard'| 'Boil'| 'As'| 'from'| 'under'| 'about'| 'If'
+NN -> 'Sugar' | 'Cardamom'| 'Saffron' | 'Gulab' | 'Jamun' | 'bowl'| 'water'| 'food'| 'processor'| 'bit'| 'powder'| 'spoon'| 'salt'| 'circle'| 'shape'| 'brown'| 'medium'| 'size'| 'mix'| 'oil'| 'frying'| 'pan'| 'mixture'| 'pakoras'| 'batch'| 'onion'| 'potatos'| 'swede'| 'heat'| 'simmer'| 'min'| 'curry'| 'chat'| 'masala'| 'stir'| 'cook'| 'rice'| 'liquid'| 'coriander'| 'cup'| 'fry'| 'fryer'| 'skin'| 'cooked'| 'garlic'| 'wait'| 'chopped'| 'chicken'| 'garam'| 'chilli'| 'corriander'| 'semolina'| 'dough'| 'hour'| 'light'| 'spongy'| 'round'| 'image'| 'sugar'| 'syrup'| 'ghee'| 'color'| 'mango'| 'flesh'| 'processor'| 'sweetener'| 'yogurt'| 'container'| 'Freeze'| 'base'| 'sorbet'| 'center'| 'fork'| 'freezer'| 'ince'| 'bowls'| 'flour'| 'pinch'| 'kg'| 'plain'| 'bt'| 'cake'| 'half'| 'galen'| 'milk'| 'custard'| 'lemon'| 'juice'| 'bottomed'| 'curd'| 'whey'| 'fat'| 'strainer'| 'line'| 'cloth'| 'muslin'| 'rinse'| 'squeeze'| 'process'| 'sourness'| 'press'| 'paneer'| 'ginger'| 'proof'| 'freeze'| 'ice'| 'cream'| 'garnish'| 'sauce'| 'bring'| 'boil'| 'till'| 'wate'| 'cardamom'| 'kheer' | 'warm'| 'cold'| 'serve'| 'egg'| 'mascarpone'| 'beat'| 'coffee'| 'cacao'| 'batter'| 'top'| 'pone'| 'lady'| 'finger'| 'tbsp'| 'dust'| 'chocolate'| 'ground'| 'potato'| 'stock'| 'tender'|  'taste'
+COMMA -> ','
+CC -> 'and'| 'or'| 'but' | '/' | '–'
+VBP -> 'leave'| 'become'| 'add'| 'want'| 'are'| 'do'| 'make'| 'attains'| 'have'| 'mix'| 'drain'
+PRP -> 'it'| 'we'| 'you'| 'them'| 'they'| 'They'| 'You'
+CD -> '24'| 'one'| '10'| '900ml'| '15'| '2'| '20'| '5'| '1'| 'two'| '4'| '30'| '40'| 'three' | '3' | '-5'
+MD -> 'can'| 'will'| 'may'
+JJ -> 'soft'| 'mashed'| 'garlic'| 'table'| 'colour'| 'golden'| 'frying'| 'Next'| 'other'| 'same'| 'ready'| 'large'| 'further'| 'chopped'| '¼'| 'mixed'| 'deep'| 'little'| 'light'| 'green'| '15-20'| 'bowl'| 'full'| 'homogeneous'| 'small'| 'delicate'| 'brown'| 'freezer-proof'| 'liquid' | 'firm' | 'fresh'| 'white'| 'hot'| 'heavy'| 'sure'| 'cold'| 'excess'| 'wrapped'| 'shallow'| 'top'| 'low'| 'mushy'| 'add'| 'open'| 'creamy'| 'cacao-mascarpone'| 'coffee-mascarpone'| 'few'| 'necessary'
+VBZ -> 'becomes'| 'is'| 'has'| 'helps'| 'Sprinkles'| 'comes'| 'takes'
+PDT -> 'all'
+VBG -> 'heating'| 'boiling'| 'bring'| 'stirring'| 'making'| 'beating'| 'scooping'| 'separating'| 'using'| 'stiring'| 'forming'| 'remaining'
+TO -> 'to'| 'To'
+VBD -> 'put'| 'sprinkled'| 'garnished'| 'chopped'| 'boiled'| 'reduced'| 'broke'| 'blitz' | 'smooth'
+RB -> 'then'| 'often'| 'together'| 'brown'| 'thoroughly'| 'carefully'| 'very'| 'Immediately'| 'still'| 'aside'| 'occasionally'| 'not'| 'gradually'| 'gently'| 'well'| 'put'| 'only'| 'milk'| 'enough'| 'up'| 'again'| 'also'| 'Then'| 'quickly'
+VBN -> 'put'| 'cooked'| 'been'| 'absorbed'| 'dried'| 'stir'| 'shown'| 'started'| 'separated'| 'done'| 'kheer'| 'softened'
+PRPS -> 'their'| 'its' | 'You'
+WRB -> 'When'
+RP -> 'up'| 'off'| 'out'
+JJS -> 'most'
+JJR -> 'more'| 'lower'
+OPEN -> '('
+CLOSE -> ')'
 """)
 
-def generate_sentence(grammar, symbol):
-    if isinstance(symbol, str):
-        productions = grammar.productions(lhs=symbol)
-        production = random.choice(productions)
-        return ' '.join(generate_sentence(grammar, sym) for sym in production.rhs())
-    else:
-        return 'bruh'
+# Initialize the parser with the grammar
+parser = RecursiveDescentParser(grammar)
 
-# Generate a sentence using the CFG
-generated_sentence = generate_sentence(grammar, grammar.start())
-print(generated_sentence)
+# Iterate over each sentence in the corpus
+for sentence in sentences:
+    # Tokenize the input sentence
+    tokenized_sentence = word_tokenize(sentence)
+
+    try:
+        print(tokenized_sentence)
+        # Check if a parse tree can be found
+        parse_tree = next(parser.parse(tokenized_sentence))
+
+        # Print the parse tree
+        tree = Tree.fromstring(str(parse_tree))
+        tree.pretty_print()
+
+        # Translate the parse tree
+        translation = ' '.join(node.label() for node in parse_tree if isinstance(node, nltk.Tree))
+
+        # Print the result
+        print("Sentence:", sentence.strip())
+        print("Translation:", translation)
+    except StopIteration:
+        print("No parse tree found for sentence:", sentence.strip())
+
+    print()
