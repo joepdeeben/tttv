434cbdc663c2cad44afa7547e90f7d316e0a0605
joepdeeben <joep.deeben@student.uva.nl>
2023-05-29 18:16:16 +0200

    verbeterde CFG om te parsen

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,6 @@
 import nltk
 from nltk import word_tokenize, CFG
-from nltk.parse import RecursiveDescentParser
+from nltk.parse import BottomUpLeftCornerChartParser
 from nltk import pos_tag
 from nltk.tree import Tree
 
@@ -13,24 +13,33 @@ sentences = raw.split('.')
 
 # Define the noun phrase pattern
 grammar = CFG.fromstring("""
-S -> VP | DT NP VP | DT NP VP CC VP
-VP -> VB | VB NP | VB NP PP | VB PP | VB ADJP | VBP | VBD | VBZ
-NP -> NN | DT NN | CD NN | JJ NN | JJ NNS | NNS | PRP | NNP | PRPS NN | NNP NN | DT NNS
-PP -> IN NP
-ADJP -> JJ | JJ CC JJ 
+S -> VP RB PRP VP
+S -> VP
+S -> VP CC VP
+S -> VP NP
+S -> NP VP
+S -> NP VP CC VP
+
+
+VP -> VB NP PP | VB VBD PP | VB IN JJ | VB IN CD | VB PRP IN | MD VP | VB JJ | VP CC VP | VB | VP CC NP
+NP -> DT NNS | DT NN | NN | NNS | DT NNS PP | JJ NN | JJ NNS | CD NNS | PRP | NP PP | PDT DT NNS | NP CC NP| DT JJ NN NN 
+PP -> IN NP 
+
+
+
 
 DT -> 'the' | 'a' | 'all' | 'Every' | 'every' | 'The' | 'This' | 'this'
 VB ->  'Return' | 'reheat' | 'Tip' | 'Seperate' | 'Stir' | 'Fill' | 'Pour' | 'Blend' | 'Keep' | 'Mix' | 'Cook' | 'Serve' | 'Cover' | 'Heat' | 'Fry' | 'Put'| 'be'| 'Make'| 'shape'| 'serve'| 'Place'| 'boil'| 'reduce'| 'Add'| 'Boil'| 'skin'| 'cut'| 'Chop'| 'cook'| 'check'| 'make'| 'separate'| 'let'| 'simmer'| 'Take'| 'soak'| 'set'| 'Beat'| 'return'| 'break'| 'Transfer'| 'fridge'| 'Buy'| 'burn'| 'stir'| 'start'| 'turn'| 'muslin'| 'Wrap'| 'take'| 'prevent'| 'scoop'| 'add'| 'cover'| 'cool'| 'have'| 'dip'| 'finish'| 'Repeat'| 'need'| 'do'| 'pan'
-NNS -> 'cheese' | 'beans'| 'hours'| 'ingredients'| 'carrots'| 'parsnips'| 'minutes'| 'cubes'| 'onions'| 'tomatoes'| 'khoya'| 'balls'| 'cups'| 'sides'| 'crystals'| 'mintures'| 'slices'| 'spoons'| 'boils'| 'cloth'| 'nuts'| 'cristals'| 'flavour'| 'pools'| 'seeds'| 'times'| 'pieces'| 'almonds'| 'pistachios'| 'yolks'| 'fingers'| 'tbsp'| 'layers'| 'mins'| 'batches'
+NNS -> 'gallon' | 'cheese' | 'beans'| 'hours'| 'ingredients'| 'carrots'| 'parsnips'| 'minutes'| 'cubes'| 'onions'| 'tomatoes'| 'khoya'| 'balls'| 'cups'| 'sides'| 'crystals'| 'mintures'| 'slices'| 'spoons'| 'boils'| 'cloth'| 'nuts'| 'cristals'| 'flavour'| 'pools'| 'seeds'| 'times'| 'pieces'| 'almonds'| 'pistachios'| 'yolks'| 'fingers'| 'tbsp'| 'layers'| 'mins'| 'batches'
 IN -> 'in'| 'of'| 'for'| 'so'| 'until'| 'after'| 'that'| 'into'| 'while'| 'with'| 'Once'| 'on'| 'if'| 'over'| 'After'| 'as'| 'For'| 'till'| 'before'| 'tard'| 'Boil'| 'As'| 'from'| 'under'| 'about'| 'If'
-NN -> 'Sugar' | 'Cardamom'| 'Saffron' | 'Gulab' | 'Jamun' | 'bowl'| 'water'| 'food'| 'processor'| 'bit'| 'powder'| 'spoon'| 'salt'| 'circle'| 'shape'| 'brown'| 'medium'| 'size'| 'mix'| 'oil'| 'frying'| 'pan'| 'mixture'| 'pakoras'| 'batch'| 'onion'| 'potatos'| 'swede'| 'heat'| 'simmer'| 'min'| 'curry'| 'chat'| 'masala'| 'stir'| 'cook'| 'rice'| 'liquid'| 'coriander'| 'cup'| 'fry'| 'fryer'| 'skin'| 'cooked'| 'garlic'| 'wait'| 'chopped'| 'chicken'| 'garam'| 'chilli'| 'corriander'| 'semolina'| 'dough'| 'hour'| 'light'| 'spongy'| 'round'| 'image'| 'sugar'| 'syrup'| 'ghee'| 'color'| 'mango'| 'flesh'| 'processor'| 'sweetener'| 'yogurt'| 'container'| 'Freeze'| 'base'| 'sorbet'| 'center'| 'fork'| 'freezer'| 'ince'| 'bowls'| 'flour'| 'pinch'| 'kg'| 'plain'| 'bt'| 'cake'| 'half'| 'galen'| 'milk'| 'custard'| 'lemon'| 'juice'| 'bottomed'| 'curd'| 'whey'| 'fat'| 'strainer'| 'line'| 'cloth'| 'muslin'| 'rinse'| 'squeeze'| 'process'| 'sourness'| 'press'| 'paneer'| 'ginger'| 'proof'| 'freeze'| 'ice'| 'cream'| 'garnish'| 'sauce'| 'bring'| 'boil'| 'till'| 'wate'| 'cardamom'| 'kheer' | 'warm'| 'cold'| 'serve'| 'egg'| 'mascarpone'| 'beat'| 'coffee'| 'cacao'| 'batter'| 'top'| 'pone'| 'lady'| 'finger'| 'tbsp'| 'dust'| 'chocolate'| 'ground'| 'potato'| 'stock'| 'tender'|  'taste'
+NN -> 'mix' | 'Sugar' | 'Cardamom'| 'Saffron' | 'Gulab' | 'Jamun' | 'bowl'| 'water'| 'food'| 'processor'| 'bit'| 'powder'| 'spoon'| 'salt'| 'circle'| 'shape'| 'brown'| 'medium'| 'size'| 'mix'| 'oil'| 'frying'| 'pan'| 'mixture'| 'pakoras'| 'batch'| 'onion'| 'potatos'| 'swede'| 'heat'| 'simmer'| 'min'| 'curry'| 'chat'| 'masala'| 'stir'| 'cook'| 'rice'| 'liquid'| 'coriander'| 'cup'| 'fry'| 'fryer'| 'skin'| 'cooked'| 'garlic'| 'wait'| 'chopped'| 'chicken'| 'garam'| 'chilli'| 'corriander'| 'semolina'| 'dough'| 'hour'| 'light'| 'spongy'| 'round'| 'image'| 'sugar'| 'syrup'| 'ghee'| 'color'| 'mango'| 'flesh'| 'processor'| 'sweetener'| 'yogurt'| 'container'| 'Freeze'| 'base'| 'sorbet'| 'center'| 'fork'| 'freezer'| 'ince'| 'bowls'| 'flour'| 'pinch'| 'kg'| 'plain'| 'bt'| 'cake'| 'half'| 'galen'| 'milk'| 'custard'| 'lemon'| 'juice'| 'bottomed'| 'curd'| 'whey'| 'fat'| 'strainer'| 'line'| 'cloth'| 'muslin'| 'rinse'| 'squeeze'| 'process'| 'sourness'| 'press'| 'paneer'| 'ginger'| 'proof'| 'freeze'| 'ice'| 'cream'| 'garnish'| 'sauce'| 'bring'| 'boil'| 'till'| 'wate'| 'cardamom'| 'kheer' | 'warm'| 'cold'| 'serve'| 'egg'| 'mascarpone'| 'beat'| 'coffee'| 'cacao'| 'batter'| 'top'| 'pone'| 'lady'| 'finger'| 'tbsp'| 'dust'| 'chocolate'| 'ground'| 'potato'| 'stock'| 'tender'|  'taste'
 COMMA -> ','
-CC -> 'and'| 'or'| 'but' | '/' | '–'
+CC -> 'and'| 'or'| 'but' | '/' | '–' | 'so' 
 VBP -> 'leave'| 'become'| 'add'| 'want'| 'are'| 'do'| 'make'| 'attains'| 'have'| 'mix'| 'drain'
 PRP -> 'it'| 'we'| 'you'| 'them'| 'they'| 'They'| 'You'
 CD -> '24'| 'one'| '10'| '900ml'| '15'| '2'| '20'| '5'| '1'| 'two'| '4'| '30'| '40'| 'three' | '3' | '-5'
 MD -> 'can'| 'will'| 'may'
-JJ -> 'soft'| 'mashed'| 'garlic'| 'table'| 'colour'| 'golden'| 'frying'| 'Next'| 'other'| 'same'| 'ready'| 'large'| 'further'| 'chopped'| '¼'| 'mixed'| 'deep'| 'little'| 'light'| 'green'| '15-20'| 'bowl'| 'full'| 'homogeneous'| 'small'| 'delicate'| 'brown'| 'freezer-proof'| 'liquid' | 'firm' | 'fresh'| 'white'| 'hot'| 'heavy'| 'sure'| 'cold'| 'excess'| 'wrapped'| 'shallow'| 'top'| 'low'| 'mushy'| 'add'| 'open'| 'creamy'| 'cacao-mascarpone'| 'coffee-mascarpone'| 'few'| 'necessary'
+JJ -> 'soft'| 'mashed'| 'garlic'| 'table'| 'colour'| 'golden'| 'frying'| 'Next'| 'other'| 'same'| 'ready'| 'smooth' | 'large'| 'further'| 'chopped'| '¼'| 'mixed'| 'deep'| 'little'| 'light'| 'green'| '15-20'| 'bowl'| 'full'| 'homogeneous'| 'small'| 'delicate'| 'brown'| 'freezer-proof'| 'liquid' | 'firm' | 'fresh'| 'white'| 'hot'| 'heavy'| 'sure'| 'cold'| 'excess'| 'wrapped'| 'shallow'| 'top'| 'low'| 'mushy'| 'add'| 'open'| 'creamy'| 'cacao-mascarpone'| 'coffee-mascarpone'| 'few'| 'necessary'
 VBZ -> 'becomes'| 'is'| 'has'| 'helps'| 'Sprinkles'| 'comes'| 'takes'
 PDT -> 'all'
 VBG -> 'heating'| 'boiling'| 'bring'| 'stirring'| 'making'| 'beating'| 'scooping'| 'separating'| 'using'| 'stiring'| 'forming'| 'remaining'
@@ -50,12 +59,15 @@ CLOSE -> ')'
 i = 0
 j = 0
 # Initialize the parser with the grammar
-parser = RecursiveDescentParser(grammar)
+parser = BottomUpLeftCornerChartParser(grammar)
 
 # Iterate over each sentence in our corpus
 for sentence in sentences:
+    # Remove commas from the input sentence
+    sentence_without_commas = sentence.replace(',', '')
+
     # Tokenize the input sentence
-    tokenized_sentence = word_tokenize(sentence)
+    tokenized_sentence = word_tokenize(sentence_without_commas)
 
     try:
         print(tokenized_sentence)
--- textgenerator.py
+++ textgenerator.py
@@ -6,27 +6,47 @@ from nltk.parse.generate import generate
 import random
 
 grammar = CFG.fromstring("""
-S -> VP | VP CC S | PRP 
-VP -> VB NP | VB NP PP | VB NP CC VP | VB NP PP CC VP
-NP -> DT JJ NNS | DT MD NNS | DT JJ NNS CC NP | DT NNS CC NP | DT CD NNS | DT JJS NNS | DT JJR NNP
-PP -> IN NP | RB
+S -> VP RB PRP VP
+S -> VP
+S -> VP CC VP
+S -> VP NP
+S -> NP VP
+S -> NP VP CC VP
 
-DT -> 'the' | 'a' | 'all' | 'every' | 'this'|
-NNS -> 'bean' | 'food' | 'garlic' | 'table' | 'salt' | 'circle' | 'oil' | 'pan' | 'onion' | 'potato' | 'swede' | 'carrot' | 'parsnip' | 'water' | 'rice' | 'coriander' | 'tomato' | 'chicken' | 'semolina' | 'khoya' | 'dough' | 'ball' | 'mango' | 'sweetener' | 'yogurt' | 'container' | 'sorbet' | 'base' | 'mixture' | 'fork' | 'bowl' | 'pinch' | 'salt' | 'flour' | 'custard' | 'galen' | 'milk' | 'lemon' | 'whey' | 'cheese' | 'cloth' | 'ginger' | 'nut' | 'rice' | 'sugar' | 'cardamom' | 'pistachio' | 'almond'
-IN ->  'in' | 'of' | 'for' | 'so' | 'until' | 'after' | 'that' | 'into' | 'while' | 'with' | 'Once' | 'on' | 'over' | 'After' | 'as' | 'For' | 'till' | 'before' | 'As' | 'from' | 'under' 
-VB -> 'leave' | 'add' | 'make' | 'fry' | 'mix' | 'heat' | 'simmer' | 'stir' | 'cook' | 'boil' | 'reduce' | 'check' | 'blend' | 'freeze' | 'beat' | 'scoop' | 'fill' | 'bring' | 'put' | 'separate' | 'beat' | 'pour' | 'turn' | 'wrap' | 'rinse' | 'squeeze' | 'transfer'
-PRP -> 'it' | 'we' | 'you' | 'them' | 'they' 
-CD -> '24 hours' | 'one' | '10' | '900ml' | '15' | '2' | '20' | '5' | '1' | 'two' | '4' | '30' | '40' | 'three'
-MD -> 'can' | 'will' | 'may'
-RB -> 'then' | 'often' | 'together' | 'thoroughly' | 'carefully' | 'very' | 'Immediately' | 'still' | 'occasionally' | 'gradually' | 'gently' | 'well' | 'only' | 'enough' | 'up' | 'again' | 'also' | 'Then' | 'quickly'
-CC -> 'and'| 'or'| 'but'
-RP -> 'up'| 'off'| 'out'
-JJ -> 'soft'| 'mashed' | 'golden'| 'frying'| 'Next'| 'other'| 'same'| 'large'| 'chopped'| 'mixed'| 'deep'| 'little'| 'light'| 'green' | 'full'| 'homogeneous'| 'small'| 'delicate'| 'brown'| 'freezer-proof'| 'liquid'| 'firm'| 'fresh'| 'white'| 'hot'| 'heavy' | 'cold'| 'excess'| 'wrapped'| 'shallow'| 'top'| 'low'| 'mushy'| 'open'| 'creamy' | 'few' | 'necessary'
+
+VP -> VB NP PP | VB VBD PP | VB IN JJ | VB IN CD | VB PRP IN | MD VP | VB JJ | VP CC VP
+NP -> DT NNS | DT NN | NN | NNS | DT NNS PP | JJ NN | JJ NNS | CD NNS | PRP | NP PP | PDT DT NNS | NP CC NP| DT JJ NN NN
+PP -> IN NP 
+
+
+
+
+DT -> 'the' | 'a' | 'all' | 'Every' | 'every' | 'The' | 'This' | 'this'
+VB ->  'Return' | 'reheat' | 'Tip' | 'Seperate' | 'Stir' | 'Fill' | 'Pour' | 'Blend' | 'Keep' | 'Mix' | 'Cook' | 'Serve' | 'Cover' | 'Heat' | 'Fry' | 'Put'| 'be'| 'Make'| 'shape'| 'serve'| 'Place'| 'boil'| 'reduce'| 'Add'| 'Boil'| 'skin'| 'cut'| 'Chop'| 'cook'| 'check'| 'make'| 'separate'| 'let'| 'simmer'| 'Take'| 'soak'| 'set'| 'Beat'| 'return'| 'break'| 'Transfer'| 'fridge'| 'Buy'| 'burn'| 'stir'| 'start'| 'turn'| 'muslin'| 'Wrap'| 'take'| 'prevent'| 'scoop'| 'add'| 'cover'| 'cool'| 'have'| 'dip'| 'finish'| 'Repeat'| 'need'| 'do'| 'pan'
+NNS -> 'gallon' | 'cheese' | 'beans'| 'hours'| 'ingredients'| 'carrots'| 'parsnips'| 'minutes'| 'cubes'| 'onions'| 'tomatoes'| 'khoya'| 'balls'| 'cups'| 'sides'| 'crystals'| 'mintures'| 'slices'| 'spoons'| 'boils'| 'cloth'| 'nuts'| 'cristals'| 'flavour'| 'pools'| 'seeds'| 'times'| 'pieces'| 'almonds'| 'pistachios'| 'yolks'| 'fingers'| 'tbsp'| 'layers'| 'mins'| 'batches'
+IN -> 'in'| 'of'| 'for'| 'so'| 'until'| 'after'| 'that'| 'into'| 'while'| 'with'| 'Once'| 'on'| 'if'| 'over'| 'After'| 'as'| 'For'| 'till'| 'before'| 'tard'| 'Boil'| 'As'| 'from'| 'under'| 'about'| 'If'
+NN -> 'Sugar' | 'Cardamom'| 'Saffron' | 'Gulab' | 'Jamun' | 'bowl'| 'water'| 'food'| 'processor'| 'bit'| 'powder'| 'spoon'| 'salt'| 'circle'| 'shape'| 'brown'| 'medium'| 'size'| 'mix'| 'oil'| 'frying'| 'pan'| 'mixture'| 'pakoras'| 'batch'| 'onion'| 'potatos'| 'swede'| 'heat'| 'simmer'| 'min'| 'curry'| 'chat'| 'masala'| 'stir'| 'cook'| 'rice'| 'liquid'| 'coriander'| 'cup'| 'fry'| 'fryer'| 'skin'| 'cooked'| 'garlic'| 'wait'| 'chopped'| 'chicken'| 'garam'| 'chilli'| 'corriander'| 'semolina'| 'dough'| 'hour'| 'light'| 'spongy'| 'round'| 'image'| 'sugar'| 'syrup'| 'ghee'| 'color'| 'mango'| 'flesh'| 'processor'| 'sweetener'| 'yogurt'| 'container'| 'Freeze'| 'base'| 'sorbet'| 'center'| 'fork'| 'freezer'| 'ince'| 'bowls'| 'flour'| 'pinch'| 'kg'| 'plain'| 'bt'| 'cake'| 'half'| 'galen'| 'milk'| 'custard'| 'lemon'| 'juice'| 'bottomed'| 'curd'| 'whey'| 'fat'| 'strainer'| 'line'| 'cloth'| 'muslin'| 'rinse'| 'squeeze'| 'process'| 'sourness'| 'press'| 'paneer'| 'ginger'| 'proof'| 'freeze'| 'ice'| 'cream'| 'garnish'| 'sauce'| 'bring'| 'boil'| 'till'| 'wate'| 'cardamom'| 'kheer' | 'warm'| 'cold'| 'serve'| 'egg'| 'mascarpone'| 'beat'| 'coffee'| 'cacao'| 'batter'| 'top'| 'pone'| 'lady'| 'finger'| 'tbsp'| 'dust'| 'chocolate'| 'ground'| 'potato'| 'stock'| 'tender'|  'taste'
+COMMA -> ','
+CC -> 'and'| 'or'| 'but' | '/' | '–' | 'so' 
+VBP -> 'leave'| 'become'| 'add'| 'want'| 'are'| 'do'| 'make'| 'attains'| 'have'| 'mix'| 'drain'
+PRP -> 'it'| 'we'| 'you'| 'them'| 'they'| 'They'| 'You'
+CD -> '24'| 'one'| '10'| '900ml'| '15'| '2'| '20'| '5'| '1'| 'two'| '4'| '30'| '40'| 'three' | '3' | '-5'
+MD -> 'can'| 'will'| 'may'
+JJ -> 'soft'| 'mashed'| 'garlic'| 'table'| 'colour'| 'golden'| 'frying'| 'Next'| 'other'| 'same'| 'ready'| 'smooth' | 'large'| 'further'| 'chopped'| '¼'| 'mixed'| 'deep'| 'little'| 'light'| 'green'| '15-20'| 'bowl'| 'full'| 'homogeneous'| 'small'| 'delicate'| 'brown'| 'freezer-proof'| 'liquid' | 'firm' | 'fresh'| 'white'| 'hot'| 'heavy'| 'sure'| 'cold'| 'excess'| 'wrapped'| 'shallow'| 'top'| 'low'| 'mushy'| 'add'| 'open'| 'creamy'| 'cacao-mascarpone'| 'coffee-mascarpone'| 'few'| 'necessary'
+VBZ -> 'becomes'| 'is'| 'has'| 'helps'| 'Sprinkles'| 'comes'| 'takes'
 PDT -> 'all'
-NNP -> 'Fry' | 'Heat' | 'Cover' | 'Serve' | 'Place' | 'Add' | 'Cook' | 'Mix' | 'Keep' | 'Saffron' | 'Sugar' | 'Blend' | 'smooth' | 'Pour' | 'Halph' | 'Fill' | 'Stir' | 'Seperate' | 'Tip' | 'Return'
+VBG -> 'heating'| 'boiling'| 'bring'| 'stirring'| 'making'| 'beating'| 'scooping'| 'separating'| 'using'| 'stiring'| 'forming'| 'remaining'
+TO -> 'to'| 'To'
+VBD -> 'put'| 'sprinkled'| 'garnished'| 'chopped'| 'boiled'| 'reduced'| 'broke'| 'blitz' | 'smooth'
+RB -> 'then'| 'often'| 'together'| 'brown'| 'thoroughly'| 'carefully'| 'very'| 'Immediately'| 'still'| 'aside'| 'occasionally'| 'not'| 'gradually'| 'gently'| 'well'| 'put'| 'only'| 'milk'| 'enough'| 'up'| 'again'| 'also'| 'Then'| 'quickly'
+VBN -> 'put'| 'cooked'| 'been'| 'absorbed'| 'dried'| 'stir'| 'shown'| 'started'| 'separated'| 'done'| 'kheer'| 'softened'
+PRPS -> 'their'| 'its' | 'You'
+WRB -> 'When'
+RP -> 'up'| 'off'| 'out'
 JJS -> 'most'
-JJR -> 'more' | 'lower'
-
+JJR -> 'more'| 'lower'
+OPEN -> '('
+CLOSE -> ')'
 """)
 
 
@@ -43,60 +63,54 @@ def extract_word_categories(grammar, symbol):
 
     return word_categories.get(symbol, [])
 
-
 # All variables turned into lists
-dt_list = extract_word_categories(grammar, 'DT')
 nns_list = extract_word_categories(grammar, 'NNS')
-in_list = extract_word_categories(grammar, 'IN')
-vb_list = extract_word_categories(grammar, 'VB')
-cc_list = extract_word_categories(grammar, 'CC')
-rp_list = extract_word_categories(grammar, 'RP')
+nn_list = extract_word_categories(grammar, 'NN')
+dt_list = extract_word_categories(grammar, 'DT')
 jj_list = extract_word_categories(grammar, 'JJ')
+vb_list = extract_word_categories(grammar, 'VB')
+in_list = extract_word_categories(grammar, 'IN')
+cc_list = extract_word_categories(grammar, 'CC')
 prp_list = extract_word_categories(grammar, 'PRP')
 cd_list = extract_word_categories(grammar, 'CD')
-md_list = extract_word_categories(grammar, 'MD')
-rb_list = extract_word_categories(grammar, 'RB')
-nnp_list = extract_word_categories(grammar, 'NNP')
-jjs_list = extract_word_categories(grammar, 'JJS')
-jjr_list = extract_word_categories(grammar, 'JJR')
 
 # Generates a random NP, using one of the rules, based on a random number
 def generate_np():
     random_number = random.randint(1, 7)
     if random_number == 1:
-        return random.choice(dt_list) + ' ' + random.choice(jj_list) + ' ' + random.choice(nns_list)
+        return random.choice(dt_list) + ' ' + random.choice(nns_list)
     elif random_number == 2:
-        return random.choice(dt_list) + ' ' + random.choice(md_list) + ' ' + random.choice(nns_list)
+        return random.choice(dt_list) + ' ' + random.choice(nn_list)
     elif random_number == 3:
-        return random.choice(dt_list) + ' ' + random.choice(jj_list) + ' ' + random.choice(nns_list) + ' ' + random.choice(cc_list) + ' ' + generate_np()
+        return random.choice(nn_list)
     elif random_number == 4:
-        return random.choice(dt_list) + ' ' + random.choice(nns_list) + ' ' + random.choice(cc_list) + ' ' + generate_np()
+        return random.choice(nns_list)
     elif random_number == 5:
-        return random.choice(dt_list) + ' ' + random.choice(cd_list) + ' ' + random.choice(nns_list)
+        return random.choice(dt_list) + ' ' + random.choice(nns_list) + ' ' + generate_pp()
     elif random_number == 6:
-        return random.choice(dt_list) + ' ' + random.choice(jjs_list) + ' ' + random.choice(nns_list)
+        return random.choice(jj_list) + ' ' + random.choice(nn_list)
     elif random_number == 7:
-        return random.choice(dt_list) + ' ' + random.choice(jjr_list) + ' ' + random.choice(nnp_list)
+        return random.choice(jj_list) + ' ' + random.choice(nns_list)
 
-# Generates a random PP or an adverb
+# Generates a random PP
 def generate_pp():
-    random_number = random.randint(1, 2)
-    if random_number == 1:
-        return random.choice(in_list) + ' ' + generate_np()
-    elif random_number == 2:
-        return random.choice(rb_list)
+    return random.choice(in_list) + ' ' + generate_np()
 
 # Generates a random VP, using one of the rules, based on a random number
 def generate_vp():
-    random_number = random.randint(1, 4)
+    random_number = random.randint(1, 6)
     if random_number == 1:
-        return random.choice(vb_list) + ' ' + generate_np()
+        return random.choice(vb_list) + ' ' + generate_np() + ' ' + generate_pp()
     elif random_number == 2:
         return random.choice(vb_list) + ' ' + generate_np() + ' ' + generate_pp()
     elif random_number == 3:
-        return random.choice(vb_list) + ' ' + generate_np() + ' ' + random.choice(cc_list) + ' ' + generate_vp()
+        return random.choice(vb_list) + ' ' + random.choice(in_list) + ' ' + random.choice(jj_list)
     elif random_number == 4:
-        return random.choice(vb_list) + ' ' + random.choice(prp_list) + ' ' + random.choice(rp_list)
+        return random.choice(vb_list) + ' ' + random.choice(in_list) + ' ' + random.choice(cd_list)
+    elif random_number == 5:
+        return random.choice(vb_list) + ' ' + random.choice(prp_list) + ' ' + random.choice(in_list)
+    elif random_number == 6:
+        return random.choice(vb_list)
 
 def generate_sentences(amount):
     sentences = []
@@ -105,10 +119,6 @@ def generate_sentences(amount):
         sentences.append(sentence)
     return sentences
 
-
 sentences = generate_sentences(50)
 for sentence in sentences:
     print(sentence)
-# with open('zinnen.txt', 'w') as zinnen:
-#     for sentence in sentences:
-#         zinnen.write(sentence + '\n')
